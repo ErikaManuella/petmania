b0a2194e0cdbbfd744a84021aefa7459b501b161
Erika Manuella <erika.manuella@academico.ifpb.edu.br>
2025-04-15 10:14:46 -0300

    definiçao de uma rota e do cabecalho padrao

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+/*<header style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>
+          <Image src="./next.svg" width="100" height={100}></Image>*/
+          <h1>PetMania</h1>
+        </header>
         {children}
       </body>
     </html>
---
+++ src/app/usuario/page.js
@@ -0,0 +1,6 @@
+export default () => (
+    <>
+        <h1>Algum texto</h1>
+        <p>Algum outro texto {10 + 3} </p>
+    </>
+)
